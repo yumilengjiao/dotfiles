6f7ea72838921e4e1ed03c27080d2c9a302e6a42
yumilengjiao <1554517392@qq.com>
2026-08-16 16:40:24 +0800

    feat: 放弃waybar配置专用noctaria

--- .config/waybar/github.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-response=$(curl -s -H "Authorization: Bearer $GITHUB_ACCESS_TOKEN_WAYBAR" https://api.github.com/notifications | jq -r '. | length')
-
-count=$response
-
-echo $count
--- .config/waybar/toggle-bluetooth.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env nu
-
-if not (bluetoothctl show | str contains "Powered: yes") {
-  bluetoothctl power on
-} else {
-  bluetoothctl power off
-}
