3f5a792ad288721a1b5c0b273559ffae218b5da1
yumilengjiao <1554517392@qq.com>
2026-08-19 22:15:15 +0800

    feat: 一些快捷键整理

---
+++ .config/nvim/lua/config/options.lua
@@ -0,0 +1,44 @@
+  vim.loader.enable()
+
+  vim.g.mapleader = ' '
+  vim.g.maplocalleader = ' '
+
+  vim.g.have_nerd_font = false
+
+  vim.o.number = true
+  vim.o.mouse = 'a'
+
+  vim.o.showmode = false
+
+  vim.schedule(function() vim.o.clipboard = 'unnamedplus' end)
+
+  vim.o.breakindent = true
+
+  vim.o.undofile = true
+
+  vim.o.smartcase = true
+
+  -- Keep signcolumn on by default
+  vim.o.signcolumn = 'yes'
+
+  -- Decrease update time
+  vim.o.updatetime = 250
+
+  -- Decrease mapped sequence wait time
+  vim.o.timeoutlen = 300
+
+  -- Configure how new splits should be opened
+  vim.o.splitright = true
+  vim.o.splitbelow = true
+
+  vim.o.list = true
+  vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+
+  vim.o.inccommand = 'split'
+
+  vim.o.cursorline = true
+
+  vim.o.scrolloff = 10
+
+  vim.o.confirm = true
+
