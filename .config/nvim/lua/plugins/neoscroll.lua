add5538b527c496895235873ebf87fd4c752f66f
yumilengjiao <1554517392@qq.com>
2026-08-24 11:39:15 +0800

    feat: 缩短neoscroll滚动动画时间

--- .config/nvim/lua/plugins/neoscroll.lua
+++ .config/nvim/lua/plugins/neoscroll.lua
@@ -4,7 +4,10 @@ local pluginName = 'karb94/neoscroll.nvim'
 local function init()
 	local utils = require('utils')
 	vim.pack.add({ utils.gh(pluginName) })
-	require('neoscroll').setup({ mappings = { '<C-u>', '<C-d>', '<C-b>', '<C-f>' } })
+	require('neoscroll').setup({
+		mappings = { '<C-u>', '<C-d>', '<C-b>', '<C-f>' },
+		duration_multiplier = 0.6,
+	})
 end
 
 PBM:register({
