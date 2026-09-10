7829d1d9275065cc8b0ca800f82689ad306cf6a0
yumilengjiao <1554517392@qq.com>
2026-09-10 13:47:23 +0800

    feat: 视同telescope替代默认

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -52,8 +52,42 @@ vim.api.nvim_create_autocmd('LspAttach', {
 			vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
 		end
 
+		local builtin = require('telescope.builtin')
+
+		-- Jump to the definition of the word under your cursor.
+		--  This is where a variable was first declared, or where a function is defined, etc.
+		--  To jump back, press <C-t>.
+		map('grd', builtin.lsp_definitions, '[G]oto [D]efinition')
+
+		-- Find references for the word under your cursor.
+		map('grr', builtin.lsp_references, '[G]oto [R]eferences')
+
+		-- Jump to the implementation of the word under your cursor.
+		--  Useful when your language has ways of declaring types without an actual implementation.
+		map('gri', builtin.lsp_implementations, '[G]oto [I]mplementation')
+
+		-- Jump to the type of the word under your cursor.
+		--  Useful when you're not sure what type a variable is.
+		map('gy', builtin.lsp_type_definitions, 'Type [D]efinition')
+
+		-- Fuzzy find all the symbols in your current document.
+		--  Symbols are things like variables, functions, types, etc.
+		map('gO', builtin.lsp_document_symbols, '[D]ocument Symbols')
+
+		-- Fuzzy find all the symbols in your current workspace.
+		--  Similar to document symbols, except searches over your entire project.
+		map('<leader>ws', builtin.lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+
+		-- Rename the variable under your cursor.
+		--  Most Language Servers support renaming across files, etc.
 		map('grn', vim.lsp.buf.rename, '[R]e[n]ame')
+
+		-- Execute a code action, usually your cursor needs to be on top of an error
+		-- or a suggestion from your LSP for this to activate.
 		map('gra', vim.lsp.buf.code_action, '[G]oto Code [A]ction', { 'n', 'x' })
+
+		-- WARN: This is not Goto Definition, this is Goto Declaration.
+		--  For example, in C this would take you to the header.
 		map('grD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
 
 		-- 光标停留高亮
