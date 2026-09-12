bb598d5182a9a8b186503a7f409b353faf6a87e1
yumilengjiao <1554517392@qq.com>
2026-09-12 20:14:44 +0800

    feat: 添加更多语言保存后自动格式化

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -11,7 +11,10 @@ local function init()
 			-- You can specify filetypes to autoformat on save here:
 			local enabled_filetypes = {
 				lua = true,
-				-- python = true,
+				python = true,
+				c = true,
+				rust = true,
+				go = true,
 			}
 			if enabled_filetypes[vim.bo[bufnr].filetype] then
 				return { timeout_ms = 500 }
@@ -25,12 +28,13 @@ local function init()
 		-- You can also specify external formatters in here.
 		formatters_by_ft = {
 			lua = { 'stylua' },
-			-- rust = { 'rustfmt' },
+			rust = { 'rustfmt' },
 			-- Conform can also run multiple formatters sequentially
-			-- python = { "isort", "black" },
+			python = { 'isort', 'black' },
 			--
+			c = { 'clang-format' },
 			-- You can use 'stop_after_first' to run the first available formatter from the list
-			-- javascript = { "prettierd", "prettier", stop_after_first = true },
+			javascript = { 'prettierd', 'prettier', stop_after_first = true },
 		},
 	})
 
